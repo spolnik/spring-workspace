fde5b774c378d75e3017bcfc4f2d6b508a69f881
Jacek Spolnik <jacek.spolnik@gmail.com>
2014-11-11 10:19:55 +0100

    - add slf4j simple dependency - add some logging code

--- my-documents/src/main/java/com/apress/isf/java/service/SearchEngineService.java
+++ my-documents/src/main/java/com/apress/isf/java/service/SearchEngineService.java
@@ -3,6 +3,8 @@ package com.apress.isf.java.service;
 import com.apress.isf.java.model.Document;
 import com.apress.isf.java.model.Type;
 import com.apress.isf.spring.data.DocumentDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,9 +12,20 @@ import java.util.stream.Collectors;
 
 public class SearchEngineService implements SearchEngine {
 
+    private static final Logger log =
+            LoggerFactory.getLogger(SearchEngineService.class);
+
     private DocumentDAO documentDAO;
 
+    public SearchEngineService() {
+        if (log.isDebugEnabled())
+            log.debug("ServiceSearchEngine created: " + this);
+    }
+
     public void setDocumentDAO(DocumentDAO documentDAO) {
+        if (log.isDebugEnabled())
+            log.debug("Document DAO set: " + documentDAO);
+
         this.documentDAO = documentDAO;
     }
 
